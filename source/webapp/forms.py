9883b5735983b0d3d442b133ebd7f188f4e43ba6
Seitek <fall3nleaf@gmail.com>
2020-08-27 18:02:01 +0600

    Добавили простую валидацию на поля ArticleForm и вывод ошибок non_field_errors в шаблоне

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
 from .models import STATUS_CHOICES
 from .models import Article, Tag
 
@@ -17,6 +19,34 @@ class ArticleForm(forms.Form):
                                     input_formats=['%Y-%m-%d', BROWSER_DATETIME_FORMAT, '%Y-%m-%dT%H:%M:%S', '%Y-%m-%d %H:%M', '%Y-%m-%d %H:%M:%S'],
                                     widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}))
     tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all(),required=False, label='Теги', widget=forms.SelectMultiple)
+
+    # def clean_title(self):
+    #     title = self.cleaned_data['title']
+    #     if len(title) < 10:
+    #         raise ValidationError('Title is too short!')
+    #     return title
+
+    # def clean_title(self):
+    #     title = self.cleaned_data['title']
+    #     if len(title) < 10:
+    #         raise ValidationError('This field should be at least %(length)d symbols long!', code='too_short',
+    #                               params={'length': 10})
+    #     return title
+
+    def clean(self):
+        cleaned_data = super().clean()
+        errors = []
+        title = cleaned_data.get('title')
+        text = cleaned_data.get('text')
+        author = cleaned_data.get('author')
+        if text and title and text == title:
+            errors.append(ValidationError("Text of the article should not duplicate it's title!"))
+        if title and author and title == author:
+            errors.append(ValidationError("Author of the article should not duplicate it's title!"))
+        if errors:
+            raise ValidationError(errors)
+        return cleaned_data
+
 
 class CommentForm(forms.Form):
     article = forms.ModelChoiceField(queryset=Article.objects.all(), required=True, label='Статья')
